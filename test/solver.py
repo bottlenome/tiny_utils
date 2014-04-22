4abfd24d13710e10d4134f835b1cf4b3888b1a76
bottlenome <bottlenome@gmail.com>
2014-04-23 07:50:51 +0900

    modify for use numpy

--- test/solver.py
+++ test/solver.py
@@ -13,6 +13,7 @@ class Point2D():
   def __init__(self, x, y):
     self.x = x
     self.y = y
+    self.np = numpy.array([self.x, self.y])
 
 class World2D():
   def __init__(self, windowWidth, windowHeight):
@@ -28,46 +29,65 @@ class World2D():
 
 class Solver():
   def __init__(self, world):
-    self.startPositions = []
-    self.finalPositions = []
     self.world = world
     self.alpha = 0.01
     self.epsilon = 0.01
     self.PointDistance = 1
-    self.finalPosition = []
     self.pointNum = 3
+    self.startPositions = None
+    self.finalPositions = None
 
   def setPointNum(self, num):
     self.pointNum = num
-  
+
   def setPointDistance(self, distance):
     self.PointDistance = distance
 
   def calcCost(self, expectation):
+    cost = 0.0
     print "TODO"
+    return cost
 
-  #TODO x,y to Point
+  #TODO remove or not
   def addStartPosition(self, point):
     self.startPosition = point
   
+  #TODO remove or not
   def addStartPositionOfSomePoint(self, point):
     self.startPositions.append(point)
   
+  #TODO remove or not
   def addFinalPosition(self, point):
-    if self.pointNum < len(self.finalPosition):
+    if self.pointNum < self.pointNum:
       self.finalPosition.append(point)
     else:
       print "err too match final position"
-  
+
+  def setStartPosition(self, startPosition):
+    self.startPositions = startPosition
+
+  def setFinalPosition(self, finalPosition):
+    self.finalPositions = finalPosition
+
   def addFinalPositionOfSomePoint(self, point):
     self.startPositions.append(point)
 
   def solve(self):
+    if self.startPositions == None:
+      print "err before solve you must set startPositions"
+      return 
+
+    if self.finalPositions == None:
+      print "err before solve you must set finalPositions"
+      return 
+    
     #set inital shape of final Position
-    expectation = []
+    expectation = numpy.array([[None, None]] * self.pointNum)
+    #TODO set finalPosition to expectation
+
     for i in range(self.pointNum):
-      if i < len(self.finalPosition):
-        expectation.append(self.finalPosition[i][:])
+      if i < len(self.finalPositions):
+        expectation.append(self.finalPositions[i][:])
       else:
         expectation.append([None, None])
       for j in range(self.world.dim):
@@ -85,8 +105,9 @@ class Solver():
 
 if __name__ == "__main__":
   world = World2D(512, 512)
-  world.show()
   solver = Solver(world)
+  solver.setStartPosition(numpy.array([Point2D(100, 0), Point2D(100, 1), Point2D(101, 0)]))
+  solver.setFinalPosition(numpy.array([Point2D(100, 0), Point2D(100, 1), Point2D(101, 0)]))
   solver.solve()
 
 
