8ab635775ae869569e4474c4047cf03726824cdd
urota <bottlenome@gmail.com>
2016-02-09 23:16:20 +0900

    add commit and push counts

--- git_util/get_commits.py
+++ git_util/get_commits.py
@@ -9,4 +9,12 @@ events = json.loads(ret)
 if len(events) == 0:
     print "no pushs for past 90 days"
 else:
-    print "TODO"
+    push_event_num = 0
+    commits_num = 0
+    for i in events:
+        if i["type"] == "PushEvent":
+            push_event_num += 1
+            commits_num += len(i["payload"]["commits"])
+    print "push events:%d"%push_event_num
+    print "commits :%d"%commits_num
+    print events
