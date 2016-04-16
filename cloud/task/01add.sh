78b7d5f457a10e635da68c32493001191c779ae2
bottlenome <bottlenome@gmail.com>
2016-04-16 17:42:39 +0900

    add new instance script

---
+++ cloud/task/01add.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+abs_path=$(cd $(dirname $0) && pwd)
+prefix_path=$(cd ${abs_path}/../ && pwd)
+
+NEWSERVERS="newservers.txt"
+
+./new_add_webnode_count.sh 2
+cat ${NEWSERVERS} | while read LINE
+do
+  echo "deploying..."${LINE}
+  ./deploy_war.sh ${LINE}
+done
+
+# update nginx
+echo "start nginx"
+
+# create nginx.conf
+echo "create nginx.conf"
+output_dir=/var/tmp
+cat ${NEWSERVERS} | while read LINE
+do
+mco facts ipaddress -F fqdn=/^web.$LINE/ -j | ${prefix_path}/bin/retrieve ip mco --format file > ${output_dir}/nginx/nginx.ipset
+done
+echo "output file: ${output_dir}/nginx/nginx.ipset"
+
+# refresh nginx.conf of lb server
+echo "refresh nginx.conf"
+mco puppetd runonce -I lb.nii.localdomain -v
+
+# restart nginx proccess
+echo "restart nginx"
+mco service nginx restart -F fqdn=/^lb/ -v
+
