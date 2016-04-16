cc035fc5e1a73bf400d5633e77db340e38793408
root <root@deploy.nii.localdomain>
2016-04-16 20:16:42 +0900

    new add

---
+++ cloud/task/02ensyu_main.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+abs_path=$(cd $(dirname $0) && pwd)
+prefix_path=/root/work/deploy
+
+OLDSERVERS="oldservers.txt"
+
+## get running web servers information
+#instance_ids=(`mco facts fqdn -F fqdn=/^web/ -j | ${prefix_path}/bin/retrieve instance_id mco`)
+#echo ${instance_ids[@]}
+
+
+echo "start"
+
+
+# read old server server id
+cat ${OLDSERVERS} | while read LINE
+do
+  echo "1_stop webnode ${LINE}"
+  ./02ensyu_sub.sh ${LINE}
+  echo "2_stop webnode ${LINE}"
+done
+
+echo "finish"
+
