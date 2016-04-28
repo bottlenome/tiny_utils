ffc730afb8a05f15b2dd293c1092fdbdae196eca
root <root@deploy.nii.localdomain>
2016-04-28 19:15:09 +0900

    modified version

--- cloud/task/02ensyu_main.sh
+++ cloud/task/02ensyu_main.sh
@@ -14,12 +14,46 @@ echo "start"
 
 
 # read old server server id
+
+<< coment
 cat ${OLDSERVERS} | while read LINE
 do
   echo "1_stop webnode ${LINE} start "
   ./02ensyu_sub.sh ${LINE}
   echo "2_stop webnode ${LINE} end "
 done
+
+do
+  echo "1_stop webnode ${LINE} start "	
+#  ./02ensyu_sub.sh ${LINE}
+  echo "2_stop webnode ${LINE} end "
+done
+
+while read LINE
+do
+  echo "1_stop webnode ${LINE} start "
+  ./02ensyu_sub.sh ${LINE}
+  echo "2_stop webnode ${LINE} end "
+done < <(cat ./oldservers.txt)
+
+for LINE in ${OLDSERVERS}
+  do
+  echo "1_stop webnode ${LINE} start "
+  ./02ensyu_sub.sh ${LINE}
+  echo "2_stop webnode ${LINE} end "
+  done
+coment
+
+count=0
+while read line; do
+	LINES[$count]=$line
+	count=$count+1
+done < ${OLDSERVERS}
+
+echo ${LINES[0]}
+echo ${LINES[1]}
+
+./02ensyu_sub.sh ${LINES[@]}
 
 echo "finish"
 
