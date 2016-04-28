f9af8d8948328d6bff5ebec74e0601c5aa3065c5
root <root@deploy.nii.localdomain>
2016-04-28 19:15:55 +0900

    add restart script

---
+++ cloud/task/04restart_oldserver.sh
@@ -0,0 +1,33 @@
+abs_path=$(cd $(dirname $0) && pwd)
+prefix_path=/root/work/deploy
+
+OLDSERVERS="oldservers.txt"
+
+cat ${OLDSERVERS} | while read LINE
+do
+  echo "restarting..."${LINE}
+  ./04sub_restart_webnode.sh ${LINE}
+done
+
+# settting for lb server
+# update nginx
+echo "start nginx"
+
+# create nginx.conf
+echo "create nginx.conf"
+output_dir=/var/tmp
+rm -f ${output_dir}/nginx/nginx.ipset
+cat ${OLDSERVERS} | while read LINE
+do
+mco facts ipaddress -F fqdn=/^web.$LINE/ -j | ${prefix_path}/bin/retrieve ip mco --format file >> ${output_dir}/nginx/nginx.ipset
+done
+echo "output file: ${output_dir}/nginx/nginx.ipset"
+
+# refresh nginx.conf of lb server
+echo "refresh nginx.conf"
+mco puppetd runonce -I lb.nii.localdomain -v
+
+# restart nginx proccess
+echo "restart nginx"
+mco service nginx restart -F fqdn=/^lb/ -v
+
