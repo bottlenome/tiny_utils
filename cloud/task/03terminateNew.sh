eecf0b9ad7dda5afb62cae7fc97d848c8a8395d6
root <root@deploy.nii.localdomain>
2016-04-30 18:36:19 +0900

    add monitor setting to terminate script

--- cloud/task/03terminateNew.sh
+++ cloud/task/03terminateNew.sh
@@ -12,3 +12,31 @@ do
     echo "terminateing..."${LINE}
     ${prefix_path}/bin/deploy instances terminate ${LINE}
 done
+
+#update monitoring condition
+mco facts ipaddress -F fqdn=/^web/ -j | ${prefix_path}/bin/retrieve ip mco --output file > ${output_dir}/monitor/web.ipset
+
+# refresh nagios config
+echo "refresh nagios config"
+mco puppetd runonce -F fqdn=/^monitor/ -v
+
+# stop ganglia-monitor
+for ipaddr in $ipaddrs; do
+  echo "stop ganglia-monitor ${ipaddr}"
+  ${prefix_path}/bin/deploy ssh exec ${ipaddr} "/etc/init.d/ganglia-monitor stop"
+done
+
+# restart ganglia-monitor
+echo "restart ganglia-monitor"
+mco service ganglia-monitor restart -F fqdn=/^web/ -v
+
+# restart gmetad
+echo "restart gmetad"
+mco service gmetad restart -F fqdn=/^monitor/ -v
+
+# restart nagios
+echo "restart nagios"
+monitor_ip=$(mco facts ipaddress -F fqdn=/^monitor/ -v | ${prefix_path}/bin/retrieve ip mco)
+${prefix_path}/bin/deploy ssh exec ${monitor_ip} "/etc/init.d/nagios3 stop"
+${prefix_path}/bin/deploy ssh exec ${monitor_ip} "/etc/init.d/nagios3 start"
+
